f0553ba9e65daffb910fc8d5dfb765872ac47f27
afiffaizun <afiffaizun31@gmail>
2026-01-15 07:29:34 +0700

    feat: update main.go and add crud

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,15 +1,33 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/afiffaizun/todo-app-cicd/internal/model"
+	"github.com/afiffaizun/todo-app-cicd/internal/handler"
+	"github.com/afiffaizun/todo-app-cicd/internal/repository"
+	"github.com/afiffaizun/todo-app-cicd/pkg/database"
+)
 
 func main() {
-	r := gin.Default()
+	db := database.NewPostgresDB("localhost", "todouser", "todopass", "tododb", "5432")
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// Auto migrate
+    db.AutoMigrate(&model.Todo{})
+    
+    // Setup handler
+    todoRepo := repository.NewTodoRepository(db)
+    todoHandler := handler.NewTodoHandler(todoRepo)
+    
+    r := gin.Default()
+    
+    // Routes
+    api := r.Group("/api/v1")
+    {
+        api.POST("/todos", todoHandler.CreateTodo)
+        api.GET("/todos", todoHandler.GetAllTodos)
+        api.PUT("/todos/:id", todoHandler.UpdateTodo)
+        api.DELETE("/todos/:id", todoHandler.DeleteTodo)
+    }
 
 	r.Run(":8080")
 }

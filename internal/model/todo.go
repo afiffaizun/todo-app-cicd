f7d1331f0694654bec5157c6e7ef42bbd39669b7
afiffaizun <afiffaizun31@gmail>
2026-01-15 21:12:29 +0700

    feat: add user model with password hashing

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -6,6 +6,8 @@ type Todo struct {
     ID          uint      `json:"id" gorm:"primaryKey"`
     Task        string    `json:"task" gorm:"not null"`
     IsCompleted bool      `json:"is_completed" gorm:"default:false"`
+    UserID      uint      `json:"user_id" gorm:"not null;index"`
+    User        User      `json:"user" gorm:"foreignKey:UserID"`
     CreatedAt   time.Time `json:"created_at"`
     UpdatedAt   time.Time `json:"updated_at"`
 }
